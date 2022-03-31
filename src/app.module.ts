a30ff298f86f01db3adb7a25179f01a3a5bfe5b1
Andrey Soares <andrey.soares.oliveira4742@gmail.com>
2022-03-31 17:21:22 -0300

    update files

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    UsersModule,
+    MongooseModule.forRoot('mongodb://localhost/nest')
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
